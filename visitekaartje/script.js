ea4c7c08dd741bcc7d2d95d46c61a87438f8bcbb
Davi9898 <Danielvink1998@gmail.com>
2023-02-06 21:51:39 +0100

    Toevoeging cards ziet er nog niet uit

--- visitekaartje/script.js
+++ visitekaartje/script.js
@@ -1,3 +1,19 @@
+const cards = document.querySelectorAll('.card'); // elementen selecteren met queryselectorAll card
+const order = [...cards].map(card => card.classList[1]); //Maakt een array // ... Spread opeartor wordt gebruikt om de Nodelist returned door QuerySAll in een Array te zetten. map method wordt gebruikt om de tweede class van elke card te extracten
+
+cards.forEach(card => { // maakt een loop die itereert 
+  card.addEventListener('click', () => { // maakt een click even listener
+    card.classList.add('animate-out'); // voegt de class animate-out toe
+    setTimeout(() => { // 1 second delay
+      card.classList.remove('animate-out'); // Hierdoor stopt de animatie
+      const currentIndex = order.indexOf(card.classList[1]); // Vind de index van de huidige card en slaat het op
+      const nextIndex = (currentIndex + 1) % order.length; // Berekent de index van de volgende card door 1 toe teveogen aan huidige Index
+      const nextCard = document.querySelector(`.${order[nextIndex]}`);
+      nextCard.parentNode.insertBefore(card, nextCard);
+    }, 1000);
+  });
+});
+
 fetch('https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/member?id=cldex6lhk48ci0auoyotb1ivk')
   .then(response => {
     if (!response.ok) {
@@ -11,5 +27,6 @@ fetch('https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/member?i
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
   });
+
 
   // cldex6lhk48ci0auoyotb1ivk ID
